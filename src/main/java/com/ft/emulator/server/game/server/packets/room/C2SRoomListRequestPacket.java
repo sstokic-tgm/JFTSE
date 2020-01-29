7d91875d0afced69897e85f6dbe083f1e627f221
sstokic-tgm <stokic95@yahoo.com>
2020-01-29 13:05:41 +0100

    matchplay stuff; roomlist handling (pagination) -> not fully finished

---
+++ src/main/java/com/ft/emulator/server/game/server/packets/room/C2SRoomListRequestPacket.java
@@ -0,0 +1,25 @@
+package com.ft.emulator.server.game.server.packets.room;
+
+import com.ft.emulator.server.game.server.Packet;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class C2SRoomListRequestPacket extends Packet {
+
+    private int roomTypeTab;
+    private char page;
+    private byte unk0;
+    private byte unk1;
+
+    public C2SRoomListRequestPacket(Packet packet) {
+
+        super(packet);
+
+        this.roomTypeTab = this.readInt();
+        this.page = this.readChar();
+        this.unk0 = this.readByte();
+	this.unk1 = this.readByte();
+    }
+}
