23063b48992de4b9d6457afb2b93fd3f9ad8a39a
sstokic-tgm <stokic95@yahoo.com>
2020-01-29 16:18:23 +0100

    GameServerList packet structure fixed

--- src/main/java/com/ft/emulator/server/game/server/packets/authserver/S2CGameServerListPacket.java
+++ src/main/java/com/ft/emulator/server/game/server/packets/authserver/S2CGameServerListPacket.java
@@ -12,16 +12,20 @@ public class S2CGameServerListPacket extends Packet {
 
         super(PacketID.S2CGameServerList);
 
-        this.write((short)gameServerList.size());
+        this.write((byte)gameServerList.size());
 
         for(GameServer gameServer : gameServerList) {
 
+            this.write((byte)0); // ?
             this.write((short)Math.toIntExact(gameServer.getId()));
             this.write(gameServer.getGameServerType().getType());
             this.write(gameServer.getHost());
             this.write((short)0);
             this.write((short)gameServer.getPort().intValue());
-            this.write(0);
+            this.write((short)0); // players online
+            this.write((byte)0); // boolean for custom channel name
+            this.write("Custom Name");
+            this.write((char)0);
 	}
     }
 }
