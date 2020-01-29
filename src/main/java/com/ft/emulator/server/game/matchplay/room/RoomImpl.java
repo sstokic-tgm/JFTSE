274e2cf5fc9976f183c89342823e77f0266aa484
sstokic-tgm <stokic95@yahoo.com>
2020-01-29 10:42:58 +0100

    roomlist now correctly refreshed if you were the only one inside

--- src/main/java/com/ft/emulator/server/game/matchplay/room/RoomImpl.java
+++ src/main/java/com/ft/emulator/server/game/matchplay/room/RoomImpl.java
@@ -2,6 +2,7 @@ package com.ft.emulator.server.game.matchplay.room;
 
 import com.ft.emulator.server.shared.module.Client;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,12 +17,17 @@ public class RoomImpl {
 
 	if(client.getActiveRoom() != null) {
 
-	    roomList.stream()
-		    .filter(r -> r.getId() == client.getActiveRoom().getId())
-		    .map(Room::getPlayerList)
-		    .flatMap(Collection::stream)
-		    .collect(Collectors.toList())
-		    .removeIf(rp -> rp.getPlayer() == client.getActiveCharacterPlayer());
+	    roomList.forEach(r -> {
+
+	        if(r.getId() == client.getActiveRoom().getId()) {
+
+	            List<RoomPlayer> toRemove = r.getPlayerList().stream()
+			    .filter(rp -> rp.getPlayer() == client.getActiveCharacterPlayer())
+			    .collect(Collectors.toList());
+
+	            r.getPlayerList().removeAll(toRemove);
+		}
+	    });
 
 	    removeRoom(roomList);
 
@@ -33,7 +39,7 @@ public class RoomImpl {
 	return roomChanges;
     }
 
-    public void removeRoom(List<Room> roomList) {
+    private void removeRoom(List<Room> roomList) {
 
 	roomList.removeIf(r -> r.getPlayerList().isEmpty());
     }
