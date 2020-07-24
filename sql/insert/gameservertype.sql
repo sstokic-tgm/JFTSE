bc751ca7d12f3a025e5d09937d8d8fbc78e00ad9
sstokic-tgm <stokic95@yahoo.com>
2020-07-25 01:03:48 +0200

    old server

--- sql/insert/gameservertype.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `fantasytennis`.`GameServerType`(`created`, `deleted`, `modified`, `name`, `type`) VALUES (NOW(), 0, NULL, 'Chat', 0);
-INSERT INTO `fantasytennis`.`GameServerType`(`created`, `deleted`, `modified`, `name`, `type`) VALUES (NOW(), 0, NULL, 'Free', 1);
-INSERT INTO `fantasytennis`.`GameServerType`(`created`, `deleted`, `modified`, `name`, `type`) VALUES (NOW(), 0, NULL, 'Rookie', 2);
