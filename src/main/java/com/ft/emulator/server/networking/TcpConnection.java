46a183c1f3f42dcc36b83ccef63ccf12c39adecb
sstokic-tgm <stokic95@yahoo.com>
2020-07-25 16:46:16 +0200

    multiple ping packets in one packet - debugging output for prod

--- src/main/java/com/ft/emulator/server/networking/TcpConnection.java
+++ src/main/java/com/ft/emulator/server/networking/TcpConnection.java
@@ -179,23 +179,7 @@ public class TcpConnection {
 	    if (readBuffer.remaining() < currentObjectLength)
 		return null;
 
-	    while (currentObjectLength == 0) {
-
-		readBuffer.position(8);
-
-		readBuffer.compact();
-		bytesRead = socketChannel.read(readBuffer);
-		readBuffer.flip();
-
-		if (bytesRead == -1)
-		    throw new SocketException("Connection is closed.");
-
-		currentObjectLength = BitKit.bytesToShort(readBuffer.array(), 6);
-
-		lastReadTime = System.currentTimeMillis();
-		if (readBuffer.remaining() < currentObjectLength)
-		    return null;
-	    }
+	    System.out.println(bytesRead + "---" + currentObjectLength);
 	}
 	else if ((bytesRead - currentObjectLength - 8) == 10) {
 
