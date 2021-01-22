79b0aa02a66520327676e1e53ac0cbc69b247a06
vince <davingi@outlook.com>
2021-01-22 23:42:42 +0100

    Define structure of experience gain info data packet

--- src/main/java/com/ft/emulator/server/game/core/packet/packets/matchplay/S2CMatchplaySetExperienceGainInfoData.java
+++ src/main/java/com/ft/emulator/server/game/core/packet/packets/matchplay/S2CMatchplaySetExperienceGainInfoData.java
@@ -8,5 +8,27 @@ public class S2CMatchplaySetExperienceGainInfoData extends Packet {
         super(PacketID.S2CMatchPlaySetExperienceGainInfoData);
 
         this.write(resultTitle); // 0 = Loser, 1 = Winner
+        this.write((byte) 0); // Unk
+
+        this.write(10); // EXP BASIC
+        this.write(20); // GOLD BASIC
+        this.write(30); // EXP BONUS
+        this.write(40); // GOLD BONUS
+        this.write(50); // EXP TOTAL
+        this.write(60); // GOLD TOTAL
+
+        this.write((byte) 20); // Unk
+        this.write((byte) 30); // Unk
+
+        this.write(30); // Playtime in seconds
+        this.write(10); // Ranking point reward
+        this.write(0); // Unk
+        this.write(0); // Unk
+        this.write(1); // Bonus
+
+        this.write((byte) 0); // Unk
+        this.write(0); // Unk
+        this.write(0); // Unk
+        this.write(0); // Unk
     }
 }
