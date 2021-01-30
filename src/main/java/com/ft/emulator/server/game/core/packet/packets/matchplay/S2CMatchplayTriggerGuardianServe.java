b8142e59f497d39603ecec0e6d20131660f9b429
vince <davingi@outlook.com>
2021-01-30 01:00:50 +0100

    Update guardian serve packet description

--- src/main/java/com/ft/emulator/server/game/core/packet/packets/matchplay/S2CMatchplayTriggerGuardianServe.java
+++ src/main/java/com/ft/emulator/server/game/core/packet/packets/matchplay/S2CMatchplayTriggerGuardianServe.java
@@ -3,12 +3,12 @@ package com.ft.emulator.server.game.core.packet.packets.matchplay;
 import com.ft.emulator.server.game.core.packet.PacketID;
 import com.ft.emulator.server.networking.packet.Packet;
 
-public class S2CMatchplayTriggerGuardianServer extends Packet {
-    public S2CMatchplayTriggerGuardianServer() {
+public class S2CMatchplayTriggerGuardianServe extends Packet {
+    public S2CMatchplayTriggerGuardianServe() {
         super(PacketID.S2CMatchplayStartGuardianServe);
 
         this.write((byte) 0); //Teamside (0=Red, 1=Blue)
         this.write((byte) 0); //X-Offset
-        this.write((byte) 0); //Unk
+        this.write((byte) 0); //Ball angle(?)
     }
 }
