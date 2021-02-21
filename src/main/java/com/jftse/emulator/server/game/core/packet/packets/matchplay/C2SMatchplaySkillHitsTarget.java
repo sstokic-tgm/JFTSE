e24037e52dcd0fad74a56b3a0a74d49d3b17acd7
vince <davingi@outlook.com>
2021-02-21 12:52:23 +0100

    Get active buffs affected on skilltargethit

--- src/main/java/com/jftse/emulator/server/game/core/packet/packets/matchplay/C2SMatchplaySkillHitsTarget.java
+++ src/main/java/com/jftse/emulator/server/game/core/packet/packets/matchplay/C2SMatchplaySkillHitsTarget.java
@@ -7,34 +7,36 @@ import lombok.Setter;
 @Getter
 @Setter
 public class C2SMatchplaySkillHitsTarget extends Packet {
+    private byte attackerPosition;
     private byte targetPosition;
     private byte skillId;
     private byte damageType;
     private int xKnockbackPosition;
     private int yKnockbackPosition;
+    private byte attackerBuffId;
+    private byte receiverBuffId;
 
     public C2SMatchplaySkillHitsTarget(Packet packet) {
         super(packet);
 
-        packet.readByte(); // Unk
+        this.attackerPosition = packet.readByte();
         packet.readByte(); // Unk
         packet.readByte(); // Unk
         this.targetPosition = packet.readByte();
         packet.readShort(); // Unk
         this.skillId = packet.readByte();
 
-        // Unknown 10 bytes (types are wrong to probably)
-        packet.readInt();
-
-        packet.readShort();
+        // 0=Str, 1=DEF, 2=Projectile speed, 3=Ball damage, 4=DMG and Projectile speed
+        // 5=Charge shot speed, 6=Movement speed, 7=Ball spin
+        this.attackerBuffId = packet.readByte();
+        packet.readShort(); // Unk
+        this.receiverBuffId = packet.readByte();
+        packet.readShort(); // Unk;
         this.damageType = packet.readByte();
-        packet.readByte();
-
-        packet.readShort();
-
+        packet.readByte(); // Unk
+        packet.readShort(); // Unk
         this.xKnockbackPosition = packet.readInt();
         this.yKnockbackPosition = packet.readInt();
-
         packet.readByte(); // Unk
     }
 }
