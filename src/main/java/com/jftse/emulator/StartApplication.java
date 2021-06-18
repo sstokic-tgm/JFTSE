2f161f4f7d78970411525f3e86fdf8402f92221d
sstokic-tgm <stokic95@yahoo.com>
2021-06-18 15:15:19 +0200

    go down from 4 to 2 threads for game server

--- src/main/java/com/jftse/emulator/StartApplication.java
+++ src/main/java/com/jftse/emulator/StartApplication.java
@@ -57,7 +57,7 @@ public class StartApplication {
         ctx.getBeanFactory().autowireBean(gameServerNetworkListener);
 
         Server gameServer = new Server();
-        gameServer.addListener(new ThreadedConnectionListener(gameServerNetworkListener, Executors.newFixedThreadPool(4)));
+        gameServer.addListener(new ThreadedConnectionListener(gameServerNetworkListener, Executors.newFixedThreadPool(2)));
 
         try {
             gameServer.bind(5895);
