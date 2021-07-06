ed18f1e9a40d0638d3c0ffa829614d6fc205bb20
sstokic-tgm <stokic95@yahoo.com>
2021-07-06 14:08:56 +0200

    new global settings

--- src/main/java/com/jftse/emulator/common/GlobalSettings.java
+++ src/main/java/com/jftse/emulator/common/GlobalSettings.java
@@ -1,8 +1,10 @@
 package com.jftse.emulator.common;
 
 public class GlobalSettings {
-    public final static boolean ShouldHandleDevPackets = false;
-    public final static boolean IsAntiCheatEnabled = true;
+    public final static boolean ShouldHandleDevPackets = true;
+    public final static boolean IsAntiCheatEnabled = false;
     public final static boolean LogAllPackets = true;
     public final static boolean TranslatePacketIds = true;
+    public final static boolean UseNetworkEncryption = false;
+    public final static boolean UsePasswordEncryption = false;
 }
