9ebbea356ec0557f3f7cd0f56073b8e4e4bc96ea
Davingi <davingi@outlook.de>
2022-03-31 19:58:10 +0200

    Make GMs in secret GM slot invisible, GMs can read team messages

--- src/main/java/com/jftse/emulator/server/core/handler/game/lobby/room/RoomReadyChangeRequestPacketHandler.java
+++ src/main/java/com/jftse/emulator/server/core/handler/game/lobby/room/RoomReadyChangeRequestPacketHandler.java
@@ -1,14 +1,18 @@
 package com.jftse.emulator.server.core.handler.game.lobby.room;
 
+import com.jftse.emulator.server.core.constants.MiscConstants;
 import com.jftse.emulator.server.core.handler.AbstractHandler;
 import com.jftse.emulator.server.core.manager.GameManager;
 import com.jftse.emulator.server.core.matchplay.room.Room;
+import com.jftse.emulator.server.core.matchplay.room.RoomPlayer;
 import com.jftse.emulator.server.core.packet.packets.lobby.room.C2SRoomReadyChangeRequestPacket;
 import com.jftse.emulator.server.core.packet.packets.lobby.room.S2CRoomPlayerInformationPacket;
 import com.jftse.emulator.server.database.model.player.Player;
 import com.jftse.emulator.server.networking.packet.Packet;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RoomReadyChangeRequestPacketHandler extends AbstractHandler {
     private C2SRoomReadyChangeRequestPacket roomReadyChangeRequestPacket;
@@ -33,10 +37,25 @@ public class RoomReadyChangeRequestPacketHandler extends AbstractHandler {
                         }
                     });
 
-            S2CRoomPlayerInformationPacket roomPlayerInformationPacket = new S2CRoomPlayerInformationPacket(new ArrayList<>(room.getRoomPlayerList()));
+            S2CRoomPlayerInformationPacket roomPlayerInformationPacket =
+                    new S2CRoomPlayerInformationPacket(new ArrayList<>(room.getRoomPlayerList()));
+
+            List<RoomPlayer> filteredRoomPlayerList = room.getRoomPlayerList().stream()
+                    .filter(x -> x.getPosition() != MiscConstants.InvisibleGmSlot)
+                    .collect(Collectors.toList());
+            S2CRoomPlayerInformationPacket roomPlayerInformationPacketWithoutInvisibleGm =
+                    new S2CRoomPlayerInformationPacket(new ArrayList<>(filteredRoomPlayerList));
+
             GameManager.getInstance().getClientsInRoom(room.getRoomId()).forEach(c -> {
-                if (c.getConnection() != null && c.getConnection().isConnected()) {
+                Long playerId = c.getPlayer().getId();
+                RoomPlayer roomPlayer = room.getRoomPlayerList().stream()
+                        .filter(rp -> rp.getPlayer().getId() == playerId)
+                        .findAny()
+                        .orElse(null);
+                if (roomPlayer != null && roomPlayer.getPosition() == MiscConstants.InvisibleGmSlot) {
                     c.getConnection().sendTCP(roomPlayerInformationPacket);
+                } else {
+                    c.getConnection().sendTCP(roomPlayerInformationPacketWithoutInvisibleGm);
                 }
             });
         }
