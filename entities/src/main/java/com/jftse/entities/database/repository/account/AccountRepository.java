0cefc89bd2ec2a9934c17d448e5437d62a9e6d5e
sstokic-tgm <stokic95@yahoo.com>
2023-02-16 17:52:39 +0100

    add findByEmail

--- entities/src/main/java/com/jftse/entities/database/repository/account/AccountRepository.java
+++ entities/src/main/java/com/jftse/entities/database/repository/account/AccountRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findAccountByUsername(String username);
     Optional<Account> findAccountById(Long id);
+    Optional<Account> findByEmail(String email);
 }
