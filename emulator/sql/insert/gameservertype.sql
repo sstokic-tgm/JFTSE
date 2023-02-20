6962e4bf3c2d74dcfc875591652f7474869691b8
sstokic-tgm <stokic95@yahoo.com>
2023-02-21 00:31:21 +0100

    update gameserver types

--- emulator/sql/insert/gameservertype.sql
+++ emulator/sql/insert/gameservertype.sql
@@ -1,4 +1,8 @@
 INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Chat', 0);
 INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Free', 1);
 INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Rookie', 2);
-INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Relay', 4);
+INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Relay', NULL);
+INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Pro', 3);
+INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Master', 4);
+INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Club', 7);
+INSERT INTO `fantasytennis`.`GameServerType`(`name`, `type`) VALUES ('Tournament', 8);
