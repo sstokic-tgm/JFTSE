697cb131e32d27ac70dbb9accae2e534663c14c8
sstokic-tgm <stokic95@yahoo.com>
2023-08-15 23:18:12 +0200

    add chat server type

--- entities/src/main/java/com/jftse/entities/database/model/ServerType.java
+++ entities/src/main/java/com/jftse/entities/database/model/ServerType.java
@@ -1,7 +1,7 @@
 package com.jftse.entities.database.model;
 
 public enum ServerType {
-    AUTH_SERVER, GAME_SERVER, RELAY_SERVER, AC_SERVER;
+    AUTH_SERVER, GAME_SERVER, CHAT_SERVER, RELAY_SERVER, AC_SERVER;
 
     public String getName() {
         return toString();
