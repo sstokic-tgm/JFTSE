ef5be08f5af1f4d33a3969f0cc641a9aaed80639
sstokic-tgm <stokic95@yahoo.com>
2025-07-14 18:35:00 +0200

    fix boss battle init

--- game-server/src/main/java/com/jftse/emulator/server/core/task/GuardianServeTask.java
+++ game-server/src/main/java/com/jftse/emulator/server/core/task/GuardianServeTask.java
@@ -10,6 +10,7 @@ import com.jftse.emulator.server.core.packets.matchplay.S2CMatchplayTriggerGuard
 import com.jftse.emulator.server.core.utils.ServingPositionGenerator;
 import com.jftse.emulator.server.net.FTClient;
 import com.jftse.emulator.server.net.FTConnection;
+import com.jftse.entities.database.model.scenario.MScenarios;
 import com.jftse.server.core.thread.AbstractTask;
 import com.jftse.server.core.thread.ThreadManager;
 import lombok.extern.log4j.Log4j2;
@@ -52,7 +53,7 @@ public class GuardianServeTask extends AbstractTask {
         GameManager.getInstance().sendPacketToAllClientsInSameGameSession(triggerGuardianServePacket, connection);
         game.resetStageStartTime();
 
-        if (game.isAdvancedBossGuardianMode()) {
+        if (game.getScenario() != null && game.getScenario().getGameMode() == MScenarios.GameMode.BOSS_BATTLE_V2) {
             final PhaseManager phaseManager = game.getPhaseManager();
             phaseManager.start(connection, game);
         }
