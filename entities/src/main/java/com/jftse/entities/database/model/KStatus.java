5d8d6fc6f44c684949b6bc5fc8cb6fefff8b9a39
sstokic-tgm <stokic95@yahoo.com>
2025-08-06 14:00:35 +0200

    add default supported values as constants

--- entities/src/main/java/com/jftse/entities/database/model/KStatus.java
+++ entities/src/main/java/com/jftse/entities/database/model/KStatus.java
@@ -12,4 +12,9 @@ import javax.persistence.Table;
 @Table(name = "K_Status")
 public class KStatus extends AbstractIdBaseModel {
     private String name;
+
+    public static Long ACTIVE = 1L;
+    public static Long INACTIVE = 2L;
+    public static Long OPEN = 3L;
+    public static Long CANCELLED = 4L;
 }
