4b8213a2384477fae30afe882307a1046e9ce72c
sstokic-tgm <stokic95@yahoo.com>
2025-10-27 19:45:10 +0100

    fix not relaying not known packets

--- relay-server/src/main/java/com/jftse/emulator/server/core/handler/RelayPacketRequestHandler.java
+++ relay-server/src/main/java/com/jftse/emulator/server/core/handler/RelayPacketRequestHandler.java
@@ -7,6 +7,7 @@ import com.jftse.server.core.handler.PacketHandler;
 import com.jftse.server.core.handler.PacketId;
 import com.jftse.server.core.protocol.IPacket;
 import com.jftse.server.core.protocol.PacketRegistry;
+import com.jftse.server.core.shared.packets.CMSGDefault;
 import com.jftse.server.core.shared.packets.relay.CMSGRelay;
 
 @PacketId(CMSGRelay.PACKET_ID)
@@ -18,7 +19,14 @@ public class RelayPacketRequestHandler implements PacketHandler<FTConnection, CM
             byte[] innerPacket = relay.getPacket();
             int packetId = BitKit.bytesToShort(innerPacket, 4);
             IPacket relayPacket = PacketRegistry.decode(packetId, innerPacket);
-            connection.queuePacket(relayPacket);
+            if (relayPacket instanceof CMSGDefault defaultPacket) {
+                // not found so its wrapped in CMSGDefault and prob not reversed yet
+                // we must still relay it because the client expects the packet to function properly
+                connection.sendTCP(defaultPacket);
+            } else {
+                // just queue the packet for processing since we know about it
+                connection.queuePacket(relayPacket);
+            }
         }
     }
 }
