7ac7d2ecab14516e1f1d6f81d65a35a7cf9dae2e
sstokic-tgm <stokic95@yahoo.com>
2025-10-29 15:53:51 +0100

    remove unused proto service; fix server start classes to encourage spring lifecycle management

--- relay-server/src/main/java/com/jftse/emulator/RelayServerStart.java
+++ relay-server/src/main/java/com/jftse/emulator/RelayServerStart.java
@@ -6,13 +6,13 @@ import com.jftse.server.core.ServerLoop;
 import com.jftse.server.core.YamlPropertySourceFactory;
 import com.jftse.server.core.protocol.PacketAutoRegister;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -21,6 +21,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.jftse.entities")
@@ -28,7 +30,7 @@ import javax.annotation.PreDestroy;
 @ComponentScan(basePackages = "com.jftse")
 @PropertySource(value = "classpath:build-info.yml", factory = YamlPropertySourceFactory.class)
 @Log4j2
-public class RelayServerStart {
+public class RelayServerStart implements CommandLineRunner {
     private static EventLoopGroup bossGroup;
     private static EventLoopGroup workerGroup;
 
@@ -38,49 +40,55 @@ public class RelayServerStart {
     @Autowired
     private RelayManager relayManager;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(RelayServerStart.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
         PacketAutoRegister.registerAll();
 
         bossGroup = new NioEventLoopGroup(1);
         workerGroup = new NioEventLoopGroup(10);
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 300)
-                    .childHandler(new ConnectionInitializer())
-                    .childOption(ChannelOption.TCP_NODELAY, true)
-                    .childOption(ChannelOption.SO_KEEPALIVE, false)
-                    .childOption(ChannelOption.SO_REUSEADDR, true)
-                    .childOption(ChannelOption.SO_RCVBUF, 16384)
-                    .childOption(ChannelOption.SO_SNDBUF, 16384);
 
-            ChannelFuture f = b.bind(5896).sync();
-            if (f.isSuccess()) {
-                ServerLoop.getInstance().start();
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 300)
+                .childHandler(new ConnectionInitializer())
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.SO_KEEPALIVE, false)
+                .childOption(ChannelOption.SO_REUSEADDR, true)
+                .childOption(ChannelOption.SO_RCVBUF, 16384)
+                .childOption(ChannelOption.SO_SNDBUF, 16384);
 
-                log.info(
-                        "\n\n**************************************\n" +
-                        "* relay-server successfully started! *\n" +
-                        "**************************************");
+        b.bind(5896).addListener(cf -> {
+            if (cf.isSuccess()) {
+                serverLoop.start();
+
+                log.info("""
+                        
+                        **************************************
+                        * relay-server successfully started! *
+                        **************************************""");
+            } else {
+                log.error("Failed to start relay-server: {}", cf.cause().getMessage(), cf.cause());
             }
-            f.channel().closeFuture().sync();
-        } finally {
-            log.info("Stopping server");
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-            ServerLoop.getInstance().stop();
-        }
+        });
     }
 
     @PreDestroy
     public void onExit() {
-        log.info("Server exited");
+        log.info("Shutting down server...");
         relayManager.onExit();
         serverLoop.stop();
-        workerGroup.shutdownGracefully();
-        bossGroup.shutdownGracefully();
+        Future<?> workerGroupFuture = workerGroup.shutdownGracefully();
+        Future<?> bossGroupFuture = bossGroup.shutdownGracefully();
+        try {
+            workerGroupFuture.get();
+            bossGroupFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Error while exiting server: {}", e.getMessage(), e);
+        }
     }
 }
