42ec81e85869e7e8867cc593e8f113dae5b62114
sstokic-tgm <stokic95@yahoo.com>
2026-05-06 16:31:28 +0200

    fix char deletion constraints

--- auth-server/src/main/java/com/jftse/emulator/server/core/handler/authentication/PlayerDeletePacketHandler.java
+++ auth-server/src/main/java/com/jftse/emulator/server/core/handler/authentication/PlayerDeletePacketHandler.java
@@ -1,5 +1,6 @@
 package com.jftse.emulator.server.core.handler.authentication;
 
+import com.jftse.emulator.server.core.manager.AuthenticationManager;
 import com.jftse.emulator.server.core.manager.ServiceManager;
 import com.jftse.emulator.server.net.FTClient;
 import com.jftse.emulator.server.net.FTConnection;
@@ -9,7 +10,10 @@ import com.jftse.entities.database.model.log.CommandLog;
 import com.jftse.entities.database.model.player.Player;
 import com.jftse.server.core.handler.PacketHandler;
 import com.jftse.server.core.handler.PacketId;
-import com.jftse.server.core.service.*;
+import com.jftse.server.core.jdbc.JdbcUtil;
+import com.jftse.server.core.service.GuildService;
+import com.jftse.server.core.service.PlayerService;
+import com.jftse.server.core.service.CommandLogService;
 import com.jftse.server.core.shared.packets.auth.CMSGPlayerDelete;
 import com.jftse.server.core.shared.packets.auth.SMSGPlayerDelete;
 import com.jftse.server.core.shared.packets.auth.SMSGPlayerList;
@@ -22,25 +26,15 @@ import java.util.List;
 @PacketId(CMSGPlayerDelete.PACKET_ID)
 public class PlayerDeletePacketHandler implements PacketHandler<FTConnection, CMSGPlayerDelete> {
     private final PlayerService playerService;
-    private final FriendService friendService;
-    private final GiftService giftService;
-    private final MessageService messageService;
-    private final ParcelService parcelService;
-    private final ProposalService proposalService;
-    private final GuildMemberService guildMemberService;
     private final GuildService guildService;
     private final CommandLogService commandLogService;
+    private final JdbcUtil jdbcUtil;
 
     public PlayerDeletePacketHandler() {
         playerService = ServiceManager.getInstance().getPlayerService();
-        friendService = ServiceManager.getInstance().getFriendService();
-        giftService = ServiceManager.getInstance().getGiftService();
-        messageService = ServiceManager.getInstance().getMessageService();
-        parcelService = ServiceManager.getInstance().getParcelService();
-        proposalService = ServiceManager.getInstance().getProposalService();
-        guildMemberService = ServiceManager.getInstance().getGuildMemberService();
         guildService = ServiceManager.getInstance().getGuildService();
         commandLogService = ServiceManager.getInstance().getCommandLogService();
+        jdbcUtil = AuthenticationManager.getInstance().getJdbcUtil();
     }
 
     @Override
@@ -104,10 +98,10 @@ public class PlayerDeletePacketHandler implements PacketHandler<FTConnection, CM
 
                     ThreadManager.getInstance().newTask(() -> {
                         try {
-                            GuildMember guildMember = guildMemberService.getByPlayer(player);
-                            if (guildMember != null) {
-                                if (guildMember.getMemberRank() != 3) {
-                                    Guild guild = guildMember.getGuild();
+                            Guild guild = guildService.findWithMembersByPlayerId(player.getId());
+                            if (guild != null) {
+                                GuildMember guildMember = guild.getMemberList().stream().filter(m -> m.getPlayer().getId().equals(player.getId())).findFirst().orElse(null);
+                                if (guildMember != null && guildMember.getMemberRank() != 3) {
                                     guild.getMemberList().removeIf(x -> x.getId().equals(guildMember.getId()));
                                     guildService.save(guild);
                                 } else {
@@ -117,17 +111,6 @@ public class PlayerDeletePacketHandler implements PacketHandler<FTConnection, CM
                                 }
                             }
 
-                            friendService.deleteAllByPlayer(player);
-                            friendService.deleteAllByFriend(player);
-                            giftService.deleteBySender(player);
-                            giftService.deleteByReceiver(player);
-                            messageService.deleteBySender(player);
-                            messageService.deleteByReceiver(player);
-                            parcelService.deleteBySender(player);
-                            parcelService.deleteByReceiver(player);
-                            proposalService.deleteBySender(player);
-                            proposalService.deleteByReceiver(player);
-
                             List<CommandLog> commandLogList = commandLogService.findAllByPlayerId(player.getId());
                             for (CommandLog commandLog : commandLogList) {
                                 commandLog.setPlayer(null);
@@ -135,7 +118,68 @@ public class PlayerDeletePacketHandler implements PacketHandler<FTConnection, CM
                                 commandLogService.save(commandLog);
                             }
 
-                            playerService.remove(player.getId());
+                            jdbcUtil.execute(em -> {
+                                em.createQuery("DELETE FROM PlayerLotteryProgress pl WHERE pl.player.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Friend f WHERE f.player.id = :playerId OR f.friend.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Gift g WHERE g.sender.id = :playerId OR g.receiver.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Message m WHERE m.sender.id = :playerId OR m.receiver.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Parcel p WHERE p.sender.id = :playerId OR p.receiver.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Proposal pr WHERE pr.sender.id = :playerId OR pr.receiver.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM PlayerPocket pp WHERE pp.pocket.id = :pocketId")
+                                        .setParameter("pocketId", player.getPocket().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM GuildGoldUsage ggu WHERE ggu.player.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM ChallengeProgress cp WHERE cp.player.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM TutorialProgress tp WHERE tp.player.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM PetStatistic ps WHERE ps.id IN (SELECT p.petStatistic.id FROM Pet p WHERE p.player.id = :playerId)")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Pet p WHERE p.player.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Player p WHERE p.id = :playerId")
+                                        .setParameter("playerId", player.getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM Pocket p WHERE p.id = :pocketId")
+                                        .setParameter("pocketId", player.getPocket().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM ClothEquipment ce WHERE ce.id = :clothEquipmentId")
+                                        .setParameter("clothEquipmentId", player.getClothEquipment().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM QuickSlotEquipment qse WHERE qse.id = :quickSlotEquipmentId")
+                                        .setParameter("quickSlotEquipmentId", player.getQuickSlotEquipment().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM ToolSlotEquipment tse WHERE tse.id = :toolSlotEquipmentId")
+                                        .setParameter("toolSlotEquipmentId", player.getToolSlotEquipment().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM SpecialSlotEquipment sse WHERE sse.id = :specialSlotEquipmentId")
+                                        .setParameter("specialSlotEquipmentId", player.getSpecialSlotEquipment().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM CardSlotEquipment cse WHERE cse.id = :cardSlotEquipmentId")
+                                        .setParameter("cardSlotEquipmentId", player.getCardSlotEquipment().getId())
+                                        .executeUpdate();
+                                em.createQuery("DELETE FROM PlayerStatistic ps WHERE ps.id = :playerStatisticId")
+                                        .setParameter("playerStatisticId", player.getPlayerStatistic().getId())
+                                        .executeUpdate();
+                            });
                         } catch (Exception e) {
                             log.error("Error while deleting player: " + e.getMessage(), e);
                             SMSGPlayerDelete playerDelete = SMSGPlayerDelete.builder().result((char) -1).build();
